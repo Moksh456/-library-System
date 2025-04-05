5b8189938322276cd4acca962681b307f76a7a81
Moxh <moxhsharma878@gmail.com>
2025-04-06 01:59:35 +0530

    adding a library class and book class

---
+++ librar.java
@@ -0,0 +1,45 @@
+
+import java.util.ArrayList;
+
+class Book{
+    public String name, author;
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
+
+class MyLibrary{
+    public ArrayList<Book> books;
+    public MyLibrary(ArrayList<Book> books) {
+        this.books = books;
+    }
+    public void addBook(Book book){
+        System.out.println("The book has been added to the library");
+        this.books.add(book);
+    }
+    public void issueBook(Book book, String issued_to){
+        System.out.println("The book has been issued from the library to " + issued_to);
+        this.books.remove(book);
+    }
+    public void returnBook(Book b){
+        System.out.println("The book has been returned");
+        this.books.add(b);
+    }
+
+}
+public class librar {
+    public static void main(String[] args) {
+  
+
+    }
+}
