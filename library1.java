c0cb59f442f3e86f83c1ccdbb03e77a81fcf8d90
Moksh <moxhsharma878@gmail.com>
2025-05-19 23:28:58 +0530

    Update library1.java

--- library1.java
+++ library1.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 class Book{
     public String name, author;
 
-    public Book(String name, String author) {
+    public Book(String name, String author) { 
         this.name = name;
         this.author = author;
     }
